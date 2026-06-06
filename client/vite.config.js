204fd5ac936a3bed8393710a7569139b633878df
Khalid Sainaro <khalid.cizdoev777@gmail.com>
2026-06-07 01:25:43 +0200

    fixed base to "/" in vite config after custom domain added

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/saas-task-manager/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
